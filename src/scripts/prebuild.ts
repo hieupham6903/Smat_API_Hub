c64bd142427f8f7307ec6616cc6c0791edab8828
hieu6903 <hieu6903az@gmail.com>
2026-04-09 18:44:47 +0700

    fix deploy

--- src/scripts/prebuild.ts
+++
@@ -1,5 +0,0 @@
-import { autoMigrateDatabase } from "../lib/auto-migrate.js";
-
-// Call generate strictly (do not push to database).
-// This generates the schema.prisma strictly for building.
-autoMigrateDatabase(true);
